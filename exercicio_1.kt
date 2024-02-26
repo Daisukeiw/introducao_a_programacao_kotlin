e38cc3828666a3f4aaca285bc8d71358a769203f
Daisukeiw <126836226+Daisukeiw@users.noreply.github.com>
2024-02-26 16:36:26 -0300

    exercicio_1.kt

---
+++ exercicio_1.kt
@@ -0,0 +1,10 @@
+/*1. Can you read the code in this program and guess what the output is (without running it in Kotlin Playground)?
+fun main() {
+    println("1")
+    println("2")
+    println("3")
+}
+R:1
+  2
+  3
+*/
